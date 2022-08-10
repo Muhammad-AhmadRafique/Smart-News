9ef9e9bac8fb77c2b31aee64fbb739c1008524ef
adnan-rolustech <34885658+adnan-rolustech@users.noreply.github.com>
2022-08-10 16:10:52 +0500

    login screen UI in progress

--- src/screens/loginScreen.js
+++ src/screens/loginScreen.js
@@ -1,27 +1,98 @@
-import React from "react";
-import { StyleSheet, View, Text, Button } from "react-native";
+import React, { useState } from "react";
+import { Alert, Image, StyleSheet, View } from "react-native";
+import { Button, Text, TextInput } from "react-native-paper";
 import styled from "styled-components/native";
+import { Spacer } from "../components/spacer.component";
 
 function LoginScreen(props) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   return (
     <View style={styles.container}>
-      {/* <Text>Hello World</Text> */}
-      <LoginButton title="Hello" />
+      <View style={{ alignItems: "center" }}>
+        <Image
+          source={require("../../assets/logo.png")}
+          style={{
+            height: 100,
+            width: 100,
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        ></Image>
+        <Spacer position="top" size="large" />
+        <Text style={styles.title}>Smart News</Text>
+      </View>
+      <Spacer position="top" size="large" />
+      <Spacer position="top" size="large" />
+      <Spacer position="top" size="large" />
+
+      <TextInput
+        label="Email"
+        value={email}
+        underlineColor="#DEDEDE"
+        activeUnderlineColor="#5f9ea0"
+        style={{ height: 55 }}
+        onChangeText={(text) => setPassword(email)}
+      />
+      <Spacer position="top" size="large" />
+      <TextInput
+        label="Password"
+        value={password}
+        underlineColor="#DDDDDD"
+        activeUnderlineColor="#5f9ea0"
+        secureTextEntry={true}
+        style={{ height: 55 }}
+        onChangeText={(text) => setPassword(text)}
+      />
+      <Spacer position="top" size="large" />
+
+      <Button
+        style={{ height: 50, justifyContent: "center" }}
+        labelStyle={{
+          color: "white",
+          fontFamily: "Lato_700Bold",
+        }}
+        color="#5f9ea0"
+        textColor="#FFFFFF"
+        backgroundColor="#C6DAF7"
+        mode="contained"
+        onPress={() => {
+          if (email === "" && password === "") {
+            Alert.alert("Error", "please enter valid credentials");
+          }
+        }}
+      >
+        SIGN IN
+      </Button>
     </View>
   );
 }
 
-export const LoginButton = styled(Button)`
-  color: ${(props) => props.theme.backgroundColor};
+export default LoginScreen;
+
+const TomatoButton = styled.Button`
+  color: tomato;
+  border-color: tomato;
+`;
+
+const Title = styled(Text)`
+  color: green;
 `;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: "column",
     backgroundColor: "white",
-    justifyContent: "center",
-    alignItems: "center",
+    marginTop: 100,
+    margin: 20,
+  },
+  btn: {
+    color: "#DDDDDD",
+  },
+  title: {
+    color: "#333333",
+    fontFamily: "Lato_700Bold",
+    fontSize: 20,
   },
 });
-
-export default LoginScreen;
