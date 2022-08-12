e597bb07f7689a40f97467cb6ec24e4e150d6d9e
adnan-rolustech <34885658+adnan-rolustech@users.noreply.github.com>
2022-08-12 15:57:49 +0500

    bottom tabbar navigation added

--- App.js
+++ App.js
@@ -1,21 +1,29 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import { StyleSheet } from "react-native";
 import React from "react";
 import LoginScreen from "./src/screens/loginScreen";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Ionicons from "@expo/vector-icons/Ionicons";
+
+import { ThemeProvider } from "styled-components";
+import { theme } from "./src/infrastructure/theme";
 
 import {
   useFonts as useLato,
   Lato_400Regular,
   Lato_700Bold,
 } from "@expo-google-fonts/lato";
-import { ThemeProvider } from "styled-components";
-import { theme } from "./src/infrastructure/theme";
-
 import {
   useFonts as useOswald,
   Oswald_400Regular,
   Oswald_700Bold,
 } from "@expo-google-fonts/oswald";
+
 import NewsFeedScreen from "./src/screens/newsFeedScreen";
+import SearchNewsFeedScreen from "./src/screens/searchNewsFeedScreen";
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   const [oswaldLoaded] = useOswald({ Oswald_400Regular, Oswald_700Bold });
@@ -28,7 +36,37 @@ export default function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
-        <NewsFeedScreen />
+        <NavigationContainer>
+          <Tab.Navigator
+            screenOptions={({ route }) => ({
+              tabBarIcon: ({ focused, color, size }) => {
+                let iconName;
+
+                if (route.name === "HeadLines") {
+                  iconName = focused ? "ios-home-sharp" : "ios-home-outline";
+                } else if (route.name === "Feeds") {
+                  iconName = focused ? "ios-search" : "search-outline";
+                }
+
+                return <Ionicons name={iconName} size={30} color={color} />;
+              },
+              tabBarShowLabel: false,
+              tabBarLabelStyle: "30px",
+              tabBarActiveTintColor: "black",
+              tabBarInactiveTintColor: "gray",
+            })}
+          >
+            <Tab.Screen
+              name="HeadLines"
+              component={NewsFeedScreen}
+              tabBarIcon
+              options={{
+                style: { justifyContent: "center", alignItems: "Center" },
+              }}
+            />
+            <Tab.Screen name="Feeds" component={SearchNewsFeedScreen} />
+          </Tab.Navigator>
+        </NavigationContainer>
       </ThemeProvider>
       <ExpoStatusBar style="auto" />
     </>
---
+++ src/screens/searchNewsFeedScreen.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { View } from "react-native";
+import { Text } from "react-native-paper";
+
+function SearchNewsFeedScreen(props) {
+  return (
+    <View>
+      <Text>Search News Feeds Screen</Text>
+    </View>
+  );
+}
+
+export default SearchNewsFeedScreen;
