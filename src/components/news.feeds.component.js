5191d04f2bd00f40cc14cfb131c4a402ddb66e1b
adnan-rolustech <34885658+adnan-rolustech@users.noreply.github.com>
2022-08-15 11:46:10 +0500

    android device issues fixed

--- src/components/news.feeds.component.js
+++ src/components/news.feeds.component.js
@@ -40,7 +40,7 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: "row",
     justifyContent: "center",
-    alignItems: "center",
+    alignItems: "flex-start",
   },
   imageContainer: {
     marginLeft: 20,
